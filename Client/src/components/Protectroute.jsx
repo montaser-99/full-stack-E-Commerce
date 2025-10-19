d414f360a106f1e3fab1cbb4daaa55a941fdf1de
Mahmoud Montaser <hicishicis112233@gmail.com>
2025-10-19 19:32:16 +0300

    Add ProtectedRoute and auto-login redirect

--- Client/src/components/Protectroute.jsx
+++ Client/src/components/Protectroute.jsx
@@ -1,12 +1,12 @@
 import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+
 
 const ProtectedRoute = ({ children }) => {
-  const user = useSelector((state) => state?.user?.userinfo);
+  
     const storedUser = localStorage.getItem("userInfo");
 
 
-  if (!user||!storedUser) {
+  if (!storedUser) {
     return <Navigate to="/login" replace />;
   }
 
